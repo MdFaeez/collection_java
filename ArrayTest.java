426973afba8f548b90ce9ae3cb6faf48c5a8f38b
Mohd Faeez <faeezrehman789@gmail.com>
2023-10-22 14:11:26 +0530

    first commit

---
+++ ArrayTest.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ArrayTest{
+
+public static void main(String args[]){
+
+ArrayList l=new ArrayList();
+l.add("shad");
+l.add("faeez");
+System.out.println(l);
+}
+}
+
+
